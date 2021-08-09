8666a40d8bac795fdceaf081d002a3d8053aa933
Md Shayon <mdshayon0@gmail.com>
2021-08-09 11:28:54 +0600

    styling with semantic ui and using some precise meetup value

--- events_manage/meetups/views.py
+++ events_manage/meetups/views.py
@@ -5,8 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     meetups = [
-        {'title': 'A first meetup'},
-        {'title': 'A second meetup'}
+        {
+            'title': 'A first meetup',
+            'location': "USA",
+            'slug': 'a-first-meet-up'
+        },
+        {
+            'title': 'A second meetup',
+            'location': "Psris",
+            'slug': 'second-meet-up'
+        }
     ]
-    return render(request, 'meetups/index.html', {'show_meetups': True,'meetups': meetups})
-
+    return render(request, 'meetups/index.html', {'show_meetups': True, 'meetups': meetups})
