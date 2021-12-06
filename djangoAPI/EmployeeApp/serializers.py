40471727e7b59815a258d41ba7fa598ac0fab9b5
Md Shayon <mdshayon0@gmail.com>
2021-12-06 08:31:17 +0600

    django rest framework serializers

---
+++ djangoAPI/EmployeeApp/serializers.py
@@ -0,0 +1,19 @@
+# SERIALIZERS BASICALLY HELP TO CONVERT COMPLEX TYPES OR MODEL INSTANCES INTO NATIVE PYTHON DATA TYPE THAT CAN THEN BE EASILY INTO JSON OR XML
+# IT'S ALSO HELP IN DESERIALIZATION
+# CONVERTING THE PAST DATA BACK TO COMPLEX TYPES
+
+from rest_framework import serializers
+from .models import Departments, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields=('DepartmentId', 'DepartmentName', )
+        
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields=('EmployeeId', 'EmployeeName', 'Department', 'DateOfJoining', 'PhotoFileName')
+        
+
+
