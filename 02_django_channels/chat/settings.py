043f610dcbd799dc656762062f4eef827b7a1a57
Md Samsuzzoha <mdshayon0@gmail.com>
2022-03-02 14:31:12 +0600

    simple server for django channels with one index view

--- 02_django_channels/chat/settings.py
+++ 02_django_channels/chat/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
-
+    'channels',
+    'chatrooms',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chat.wsgi.application'
+ASGI_APPLICATION = 'chat.asgi.application'
 
 
 # Database
