fc48d06f7e4e0119ddfbb4a2e131d7017c4a930d
Md Shayon <mdshayon0@gmail.com>
2023-07-19 06:28:30 +0600

    [Till-01:17:00] - User registration (Serializer and view)

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from accounts.models import User 
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+    
+    class Meta:
+        model = User 
+        fields = "__all__"
+        # fields = ["email", "name", "password", "password2", "tc"]
+        extra_kwargs = {
+            "password2": {"write_only": True}
+        }
+
+    def validate(self, attrs):
+        password = attrs.get("password")
+        password2=attrs.pop('password2')
+        if password != password2:
+            raise serializers.ValidationError('Password did not match!')
+        return attrs
+    
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+
+class UserRegistrationView(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            print(user)
+            return Response({"message": "Registration success!"}, status=status.HTTP_201_CREATED)
+        return Response({"message": serializer.error_messages}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    
